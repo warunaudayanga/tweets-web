7fa7b38dff7c8b246e86a7ff24939bca4c206bc3
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2025-08-03 17:06:13 +0530

    Fix issue with ui

--- src/app/components/tweet-feed/tweet-feed.component.ts
+++ src/app/components/tweet-feed/tweet-feed.component.ts
@@ -1,53 +1,57 @@
+/* eslint-disable */
 import { CommonModule, DatePipe } from "@angular/common";
-import { EntityId } from "../../core/types";
-import { Component, computed, inject, OnInit, Signal, signal } from "@angular/core";
-import { AuthState } from "../../core/state";
-import { Tweet, TweetLike } from "../../core/interfaces";
+import { Component, Signal, WritableSignal, computed, inject, signal, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { TweetService } from "../../core/services";
-import { CreateTweetReplyDto } from "../../core/interfaces/dtos";
+import { Tweet, TweetLike } from "../../core/interfaces";
+import { CreateTweetReplyDto, CreateTweetDto } from "../../core/interfaces/dtos";
+import { EntityId } from "../../core/types";
+import { AuthState } from "../../core/state";
+
+type Tab = "all" | "my" | "create";
 
 @Component({
     selector: "app-tweet-feed",
     standalone: true,
     templateUrl: "./tweet-feed.component.html",
     styleUrls: ["./tweet-feed.component.scss"],
-    imports: [CommonModule, DatePipe],
+    imports: [CommonModule, FormsModule, DatePipe],
 })
 export class TweetFeedComponent implements OnInit {
     authState = inject(AuthState);
 
-    readonly tweets = signal<Tweet[]>([]);
+    tweetService = inject(TweetService);
 
-    readonly visibleReplies = signal<Set<EntityId>>(new Set());
+    readonly tweets: WritableSignal<Tweet[]> = signal<Tweet[]>([]);
 
-    readonly replyInputs = signal<Record<EntityId, string>>({});
+    readonly visibleReplies: WritableSignal<Set<EntityId>> = signal(new Set());
 
-    readonly showAllTweets = signal(true);
+    readonly replyInputs: WritableSignal<Record<EntityId, string>> = signal({});
 
-    constructor(private readonly tweetService: TweetService) {}
+    readonly tab: WritableSignal<Tab> = signal("all");
+
+    newTweetContent = "";
 
     ngOnInit(): void {
-        this.setShowAllTweets(true);
+        this.selectTab("all");
     }
 
-    setShowAllTweets(value: boolean): void {
-        this.showAllTweets.set(value);
-        const source$ = value ? this.tweetService.getAll() : this.tweetService.getMyTweets();
+    selectTab(tab: Tab): void {
+        this.tab.set(tab);
 
-        source$.subscribe({
-            next: tweets => this.tweets.set(tweets),
-            // eslint-disable-next-line no-console
-            error: err => console.error(err),
-        });
+        if (tab !== "create") {
+            const source$ = tab === "all" ? this.tweetService.getAll() : this.tweetService.getMyTweets();
+
+            source$.subscribe({
+                next: tweets => this.tweets.set(tweets),
+                error: err => console.error(err),
+            });
+        }
     }
 
     toggleReplies(id: EntityId): void {
         const current = new Set(this.visibleReplies());
-        if (current.has(id)) {
-            current.delete(id);
-        } else {
-            current.add(id);
-        }
+        current.has(id) ? current.delete(id) : current.add(id);
         this.visibleReplies.set(current);
     }
 
@@ -62,19 +66,16 @@ export class TweetFeedComponent implements OnInit {
         if (!content) return;
 
         const dto: CreateTweetReplyDto = { content, tweetId: id };
-
         this.tweetService.replyToTweet(id, dto).subscribe({
             next: reply => {
                 const updatedTweets = this.tweets().map(tweet =>
                     tweet.id === id ? { ...tweet, replies: [...(tweet.replies || []), reply] } : tweet,
                 );
                 this.tweets.set(updatedTweets);
-
                 const inputs = { ...this.replyInputs() };
                 delete inputs[id];
                 this.replyInputs.set(inputs);
             },
-            // eslint-disable-next-line no-console
             error: err => console.error(err),
         });
     }
@@ -88,31 +89,26 @@ export class TweetFeedComponent implements OnInit {
                     const updatedLikes = alreadyLiked
                         ? tweet.likes?.filter(l => l.userId !== this.authState.userId()) || []
                         : [...(tweet.likes || []), like];
-
                     return { ...tweet, likes: updatedLikes };
                 });
                 this.tweets.set(updatedTweets);
             },
-            // eslint-disable-next-line no-console
             error: err => console.error(err),
         });
     }
 
     deleteTweet(id: EntityId): void {
-        // eslint-disable-next-line no-alert
         if (!window.confirm("Are you sure you want to delete this tweet?")) return;
 
         this.tweetService.delete(id).subscribe({
             next: () => {
                 this.tweets.set(this.tweets().filter(tweet => tweet.id !== id));
             },
-            // eslint-disable-next-line no-console
             error: err => console.error(err),
         });
     }
 
     deleteReply(tweetId: EntityId, replyId: EntityId): void {
-        // eslint-disable-next-line no-alert
         if (!window.confirm("Are you sure you want to delete this reply?")) return;
 
         this.tweetService.deleteReply(tweetId, replyId).subscribe({
@@ -124,7 +120,22 @@ export class TweetFeedComponent implements OnInit {
                 });
                 this.tweets.set(updatedTweets);
             },
-            // eslint-disable-next-line no-console
+            error: err => console.error(err),
+        });
+    }
+
+    createTweet(): void {
+        const content = this.newTweetContent.trim();
+        if (!content) return;
+
+        const dto: CreateTweetDto = { content };
+
+        this.tweetService.create(dto).subscribe({
+            next: tweet => {
+                this.tweets.set([tweet, ...this.tweets()]);
+                this.newTweetContent = "";
+                this.selectTab("all"); // Optionally auto-switch to All Tweets
+            },
             error: err => console.error(err),
         });
     }
